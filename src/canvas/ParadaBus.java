582f309653d703010d45aee1f108c001ceb433a1
oscar chavarriaga <oscar.chavarriaga@correounivalle.edu.co>
2012-12-13 16:45:59 -0500

    Agrego paquete de implementación para pintar las rutas "Canvas"

---
+++ src/canvas/ParadaBus.java
@@ -0,0 +1,49 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package canvas;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ParadaBus {
+    private String idEstacion;
+    private int x;
+    private int y;
+
+    public ParadaBus() {
+    }
+
+    public ParadaBus(String idEstacion, int x, int y) {
+        this.idEstacion = idEstacion;
+        this.x = x;
+        this.y = y;
+    }
+
+    public String getIdEstacion() {
+        return idEstacion;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setIdEstacion(String idEstacion) {
+        this.idEstacion = idEstacion;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+}
